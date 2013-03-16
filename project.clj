9af8e5509d87652667ae22640e6b08f0222ed6d4
Richard Hull <rm_hull@yahoo.co.uk>
2013-03-16 19:42:55 +0000

    Update dependency versions

--- project.clj
+++ project.clj
@@ -2,11 +2,11 @@
   :url "http://turmites.destructuring-bind.org"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [noir "1.3.0"]
-                 [jayq "2.0.0"]
+                 [jayq "2.1.0"]
                  [rm-hull/ring-gzip-middleware "0.1.4-SNAPSHOT"]
-                 [rm-hull/monet "0.1.4-SNAPSHOT"]]
+                 [rm-hull/monet "0.1.6-SNAPSHOT"]]
   :cljsbuild
   {:builds
    [{:source-paths ["src/turmites/client"],
@@ -17,7 +17,7 @@
       :optimizations :advanced,
       :print-input-delimiter true}}]}
   :hooks [leiningen.cljsbuild]
-  :plugins [[lein-cljsbuild "0.3.0"]]
+  :plugins [[rm-hull/lein-cljsbuild "0.3.1-SNAPSHOT"]]
   :profiles {:dev {:dependencies [[vimclojure/server "2.3.6"]]}}
   :main turmites.server
   :min-lein-version "2.0.0"
